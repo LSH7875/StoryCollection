f0cfd87edc679af2e42929c0c4fb60e928258cda
LSH7875 <tmdgml7875@gmail.com>
2021-08-14 15:42:51 +0900

    useEffect추가

--- front/pages/index.jsx
+++ front/pages/index.jsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import {useRouter} from 'next/router'
 import Link from 'next/link'
 import Store from '../store/context'
+import {useContext,useEffect} from 'react'
 
 const P =Styled.h1`
     margin-top:3rem;
